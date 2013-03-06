80b071758b9823368b8ac6cf7097633a8cbea435
Andrew W Chellinsky <andrew@chellinsky.com>
2013-03-05 23:27:48 -0800

    Fixed regex and updated test to check valid profile names work.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -32,12 +32,20 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+    user = User.new(first_name: 'Andrew', last_name: 'Chellinsky', email: 'chellinsky@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
   	user.profile_name = "My Profile Name With Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Andrew', last_name: 'Chellinsky', email: 'chellinsky@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = 'chellinsky_1'
+    assert user.valid?
   end
   
 end
